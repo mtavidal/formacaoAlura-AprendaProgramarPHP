035da6d3088803022c2fa6da3600e1e9870ff0b6
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-08-16 10:45:06 -0300

    Inicia curso 02

---
+++ curso02/listas.php
@@ -0,0 +1,7 @@
+<?php
+//array ou vetor, possui indices/chaves
+$idadeList = [21, 58, 45, 85, 69];
+$algumaIdade = $idadeList[2];
+
+
+echo $algumaIdade;
---
+++ curso02/loop-lista.php
@@ -0,0 +1,11 @@
+<?php
+$idadeList = [21, 58, 45, 85, 69];
+for ($i = 0; $i < count($idadeList); $i++){
+    echo $idadeList[$i]  . PHP_EOL;
+}
+
+
+$nomeList = array("João", "Maria", "Pedro", "Ana");
+for ($i = 0; $i < count($nomeList); $i++){
+    echo $nomeList[$i]  . PHP_EOL;
+}
